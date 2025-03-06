1b94676246ec077848a596ed04e3c55f91771da4
Azeemkureshi <124749267+Azeemkureshi@users.noreply.github.com>
2025-03-06 13:14:31 +0530

    Update plyrimbd.js

--- plyrimbd.js
+++ plyrimbd.js
@@ -11,6 +11,13 @@ document.addEventListener("DOMContentLoaded", function () {
             if (imdbTitleMatch && imdbTitleMatch[1]) {
                 const imdbTitle = imdbTitleMatch[1];
 
+                // Naya div create karein
+                const iframeContainer = document.createElement("div");
+                iframeContainer.style.textAlign = "center";
+                iframeContainer.style.marginTop = "20px";
+
+                const heading = document.createElement("h3");
+                heading.innerText = "Watch online";
 
                 // Iframe wrapper for responsive aspect ratio
                 const iframeWrapper = document.createElement("div");
